598143847959ef41538add21622f9098f754d66a
Hannah Duong <hannah.n.duong@gmail.com>
2026-03-17 22:24:28 -0400

    idk

---
+++ server/dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.post("/scan-secure", (req, res) => {
+    const { e, c } = req.body;
+    if (!e || !c) {
+        return res.status(400).json({ message: "Invalid scan" });
+    }
+    return res.json({ message: "Scan successful!" });
+});
+app.listen(4000, () => {
+    console.log("Server running on http://localhost:4000");
+});
